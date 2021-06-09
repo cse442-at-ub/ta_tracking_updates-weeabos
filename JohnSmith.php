f22a913abcfc06f3793dc77eea3fd1435d773cbc
shreyaupadhya <shreyaup@buffalo.edu>
2021-06-09 17:48:11 -0400

    Create a table of all TA's for a course

---
+++ JohnSmith.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  width: 100%;
+}
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
+</head>
+<body>
+<h2> John Smith Availability  </h2>
+<table>
+  <tr>
+    <th>Dates </th>
+    <th>Locations</th>
+  </tr>
+  <tr>
+    <td> 06/05/2021</td>
+    <td> Davis Hall </td>
+  </tr>
+  <tr>
+    <td>06/07/2021</td>
+    <td>Online</td>
+    </tr>
+</table>
+</body>
+</html>
