50a4b9c3a8db68443d0ad080eb9857a2ce69196a
anikaleg <anikaleg@buffalo.edu>
2021-06-09 18:51:45 -0400

    Create login page without restrictions

---
+++ loginPage.php
@@ -0,0 +1,14 @@
+<html>
+
+<h1> Teaching Assistant Availability System </h1>
+<h2> Professor Login </h2>
+<body>
+<form action="courses.php" method="post">
+UBIT ID: <input type="text" name="id">
+<br>
+<input type="submit">
+</form>
+
+
+</body>
+</html>
