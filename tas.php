c2a7a87c26d7a5f64166443f4d95c27d2ded1682
shreyaupadhya <shreyaup@buffalo.edu>
2021-06-09 17:44:39 -0400

    Create TA info page

---
+++ tas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;
+}
+</style>
+</head>
+<body>
+
+
+<table style="width:100%">
+  
+    <th><a href="JohnSmith.php"> John Smith </a></th>
+    <th><a href= "JohnSmith.php"> Madison Wright </a></th>
+    <th><a href="JohnSmith.php"> Ragu Raj </a></th>
+    
+
+</table>
+
+</body>
+</html>
