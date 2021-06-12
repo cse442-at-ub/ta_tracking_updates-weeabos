a754d712e0b25bbeaab8ced3e0987a2ae3053b8a
anikaleg <anikaleg@buffalo.edu>
2021-06-12 14:44:29 -0400

    Create locationSchedule page and connect to database #30

--- locations.php
+++ locations.php
@@ -1,23 +1,33 @@
+<!DOCTYPE html>
 <html>
-<head>
-<style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 5px;
-  text-align: left;
-}
-</style>
-</head>
-<body>
-<table style="width:100%">
-  
-    <th><a href="online.php"> Online </a></th>
-    <th><a href= "online.php"> Davis Hall </a></th>
+<h1>
+Locations
+</h1>
 
-</table>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+$sql = "SELECT DISTINCT location FROM sessions";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) ?> {
+         <a href="locationSchedule.php">$row["location"]</a>;
+    }
+<?php
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
 
 </body>
 </html>
