68b2d1cb55c92a14e4f82755bc60f73060d6dca9
anikaleg <anikaleg@buffalo.edu>
2021-06-14 21:03:24 -0400

    Fix syntax of select statement #29

--- locations.php
+++ locations.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
 <?php
 session_start();
 ?>
+
+<!DOCTYPE html>
 <html>
 <h1>
-Locations
+Locations:
 </h1>
 
 <body>
 
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "";
+$dbname = "oceanus";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-$sql = "SELECT DISTINCT location FROM sessions WHERE course=$_SESSION["course"]";
+$sql = "SELECT DISTINCT location FROM sessions WHERE course='{$_SESSION['course']}'";
 $result = mysqli_query($conn, $sql);
+$count = mysqli_num_rows($result);
 
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) ?> {
-         <a href="locationSchedule.php" <?php $_SESSION ["location"] = $row["location"];?>> <?php $row["location"]; ?> </a>;
-    }
-<?php
+if ($count > 0) {
+    while($row = mysqli_fetch_assoc($result)) {  ?>
+         <a href="locationSchedule.php" <?php $_SESSION["location"] = $row["location"];?>> <?php echo $row["location"]; ?> </a>
+<br> </br>
+  <?php }
+
 } else {
     echo "0 results";
 }
