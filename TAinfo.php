4c74bd86440f11f22677d562dace79766baf9de7
kyle <kyledpaul00@gmail.com>
2021-06-23 00:12:12 -0400

    added sql injection and xss security. retrieving information for selected TA does not work

--- TAinfo.php
+++ TAinfo.php
@@ -1,4 +1,9 @@
 <?php
+
+if (
+  (!isset($_SERVER['HTTPS'])||($_SERVER['HTTPS']!='on')))
+{header('Location: '. 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']);}
+
 session_start();
 ?>
 <html>
@@ -29,9 +34,11 @@ $password = "50260751";
 $dbname = "cse442_2021_summer_team_c_db";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-$sql = "SELECT DISTINCT registered_users.first_name, registered_users.last_name, registered_users.email, registered_users.faculty, office_hours.email, office_hours.course FROM registered_users INNER JOIN office_hours  ON office_hours.email = registered_users.email WHERE office_hours.course='{$_SESSION['courseSelected']}' AND registered_users.faculty = 0";
-$result = mysqli_query($conn, $sql);
+$course = $_SESSION['courseSelected'];
+$sql = $conn->prepare("SELECT DISTINCT registered_users.first_name, registered_users.last_name, registered_users.email, registered_users.faculty, office_hours.email, office_hours.course FROM registered_users INNER JOIN office_hours  ON office_hours.email = registered_users.email WHERE office_hours.course= ? AND registered_users.faculty = 0");
+$sql->bind_param("s", $course);
+$sql->execute();
+$result = $sql->get_result();
 $count = mysqli_num_rows($result);
 ?>
 <form>
--- TAschedule.php
+++ TAschedule.php
@@ -1,4 +1,9 @@
 <?php
+
+if (
+  (!isset($_SERVER['HTTPS'])||($_SERVER['HTTPS']!='on')))
+{header('Location: '. 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']);}
+
 session_start();
 
 ?>
@@ -30,8 +35,10 @@ $password = "50260751";
 $dbname = "cse442_2021_summer_team_c_db";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$sql = "SELECT  * FROM office_hours WHERE email = '".$q."' AND course='{$_SESSION['courseSelected']}'";
-$result = mysqli_query($conn, $sql);
+$course = $_SESSION['courseSelected'];
+$sql = $conn->prepare( "SELECT  * FROM office_hours WHERE email = ? AND course= ?");
+$sql->bind_param("ss", $q, $course);
+$result = $sql->get_result();
 $count = mysqli_num_rows($result);
 
 echo "<table>
