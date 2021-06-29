4a08add798aa681fc94022553c59455e002f5fdc
anikaleg <anikaleg@buffalo.edu>
2021-06-29 11:49:47 -0400

    Update visuals of login page #35

--- TAinfo.php
+++ TAinfo.php
@@ -1,5 +1,4 @@
 <?php
-
 if (
   (!isset($_SERVER['HTTPS'])||($_SERVER['HTTPS']!='on')))
 {header('Location: '. 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']);}
--- login.php
+++ login.php
@@ -48,12 +48,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <html>
 
-<h1> Teaching Assistant Availability System </h1>
-<h2> Professor Login </h2>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 
+<style>
+body {background-image: linear-gradient(rgba(0,0,0,.75), rgba(0,0,0,1)), url("https://upload.wikimedia.org/wikipedia/commons/1/1d/Alumni_Arena_%28UB%29.jpg");}
+h1   {color: white;}
+h2   {color: white;}
+form {color: white;}
+</style>
+
+<br> </br>
+<h1 class="text-center"> University at Buffalo </h1>
+<h2 class="text-center"> Teaching Assistant Availability System </h2>
+<br> </br>
 <body>
-<form action = "" method = "post">
-<label>UBIT ID :</label><input type = "text" name = "ubit_id" class = "box"/><br /><br />
+<form action = "" method = "post" class="text-center">
+<label>UBIT ID: </label><input type = "text" name = "ubit_id" class = "box"/><br /><br />
 <input type = "submit" value = " Submit "/><br />
 </form>
 </body>
